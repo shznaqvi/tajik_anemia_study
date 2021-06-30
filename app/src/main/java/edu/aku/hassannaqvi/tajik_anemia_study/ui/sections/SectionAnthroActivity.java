29eade5bc09beb8d303321b109e019dfdd323079
SAJID LATIF <indus1530@gmail.com>
2021-07-01 00:33:13 +1400

    SectionAnthroActivity Added

---
+++ app/src/main/java/edu/aku/hassannaqvi/tajik_anemia_study/ui/sections/SectionAnthroActivity.java
@@ -0,0 +1,101 @@
+package edu.aku.hassannaqvi.tajik_anemia_study.ui.sections;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.validatorcrawler.aliazaz.Validator;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import edu.aku.hassannaqvi.tajik_anemia_study.R;
+import edu.aku.hassannaqvi.tajik_anemia_study.contracts.TableContracts;
+import edu.aku.hassannaqvi.tajik_anemia_study.core.MainApp;
+import edu.aku.hassannaqvi.tajik_anemia_study.database.DatabaseHelper;
+import edu.aku.hassannaqvi.tajik_anemia_study.databinding.ActivitySectionAnthroBinding;
+import edu.aku.hassannaqvi.tajik_anemia_study.models.Form;
+import edu.aku.hassannaqvi.tajik_anemia_study.ui.EndingActivity;
+
+import static edu.aku.hassannaqvi.tajik_anemia_study.core.MainApp.form;
+
+
+public class SectionAnthroActivity extends AppCompatActivity {
+    ActivitySectionAnthroBinding bi;
+    private DatabaseHelper db;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_anthro);
+        bi.setCallback(this);
+        setupSkips();
+    }
+
+
+    private void setupSkips() {
+
+    }
+
+
+    private boolean updateDB() {
+        db = MainApp.appInfo.getDbHelper();
+        long updcount = db.addForm(form);
+        form.setId(String.valueOf(updcount));
+        if (updcount > 0) {
+            form.setUid(form.getDeviceId() + form.getId());
+            db.updatesFormColumn(TableContracts.FormsTable.COLUMN_UID, form.getUid());
+            return true;
+        } else {
+            Toast.makeText(this, "Updating Database... ERROR!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+    public void btnContinue() {
+        if (!formValidation()) return;
+        saveDraft();
+        if (updateDB()) {
+            finish();
+            startActivity(new Intent(this, EndingActivity.class).putExtra("complete", true));
+        } else {
+            Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void saveDraft() {
+        MainApp.form = new Form();
+
+        form.setUserName(MainApp.user.getUserName());
+        form.setSysDate(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH).format(new Date().getTime()));
+        form.setDeviceId(MainApp.deviceid);
+        form.setAppver(MainApp.versionName + "." + MainApp.versionCode);
+
+    }
+
+
+    public void btnEnd() {
+        finish();
+        startActivity(new Intent(this, EndingActivity.class).putExtra("complete", false));
+    }
+
+
+    private boolean formValidation() {
+        return Validator.emptyCheckingContainer(this, bi.GrpName);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(this, "Back Press Not Allowed", Toast.LENGTH_SHORT).show();
+    }
+
+
+}
