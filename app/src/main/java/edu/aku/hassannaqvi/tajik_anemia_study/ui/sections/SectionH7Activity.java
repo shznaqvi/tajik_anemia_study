eaedb6c5d044fff2c23bf18667ed9e89dab1ce03
ahs1n <ahsanpervez74@gmail.com>
2021-07-05 12:34:43 +0500

    SectionH7Activity Add Skips

--- app/src/main/java/edu/aku/hassannaqvi/tajik_anemia_study/ui/sections/SectionH7Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/tajik_anemia_study/ui/sections/SectionH7Activity.java
@@ -2,6 +2,7 @@ package edu.aku.hassannaqvi.tajik_anemia_study.ui.sections;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +23,7 @@ import edu.aku.hassannaqvi.tajik_anemia_study.models.Form;
 import edu.aku.hassannaqvi.tajik_anemia_study.ui.EndingActivity;
 
 import static edu.aku.hassannaqvi.tajik_anemia_study.core.MainApp.form;
+import static edu.aku.hassannaqvi.tajik_anemia_study.utils.DateUtilsKt.rgLsnr;
 
 
 public class SectionH7Activity extends AppCompatActivity {
@@ -39,6 +41,21 @@ public class SectionH7Activity extends AppCompatActivity {
 
 
     private void setupSkips() {
+
+        rgLsnr(bi.h701, bi.h701b, new ViewGroup[]{bi.fldGrpCVh701t1, bi.fldGrpCVh701t2});
+        rgLsnr(bi.h702, bi.h702b, new ViewGroup[]{bi.fldGrpCVh702t1, bi.fldGrpCVh702t2});
+        rgLsnr(bi.h703, bi.h703b, new ViewGroup[]{bi.fldGrpCVh703t1, bi.fldGrpCVh703t2});
+        rgLsnr(bi.h704, bi.h704b, new ViewGroup[]{bi.fldGrpCVh704t1, bi.fldGrpCVh704t2});
+        rgLsnr(bi.h705, bi.h705b, new ViewGroup[]{bi.fldGrpCVh705t1, bi.fldGrpCVh705t2});
+        rgLsnr(bi.h706, bi.h706b, new ViewGroup[]{bi.fldGrpCVh706t1, bi.fldGrpCVh706t2});
+        rgLsnr(bi.h707, bi.h707b, new ViewGroup[]{bi.fldGrpCVh707t1, bi.fldGrpCVh707t2});
+        rgLsnr(bi.h708, bi.h708b, new ViewGroup[]{bi.fldGrpCVh708t1, bi.fldGrpCVh708t2});
+        rgLsnr(bi.h709, bi.h709b, new ViewGroup[]{bi.fldGrpCVh709t1, bi.fldGrpCVh709t2});
+        rgLsnr(bi.h710, bi.h710b, new ViewGroup[]{bi.fldGrpCVh710t1, bi.fldGrpCVh710t2});
+        rgLsnr(bi.h711, bi.h711b, new ViewGroup[]{bi.fldGrpCVh711t1, bi.fldGrpCVh711t2});
+        rgLsnr(bi.h712, bi.h712b, new ViewGroup[]{bi.fldGrpCVh712t1, bi.fldGrpCVh712t2});
+        rgLsnr(bi.h713a, bi.h713ab, new ViewGroup[]{bi.fldGrpCVh713bt, bi.fldGrpCVh713ct, bi.fldGrpCVh713dt});
+        rgLsnr(bi.h714a, bi.h714ab, new ViewGroup[]{bi.fldGrpCVh714bt, bi.fldGrpCVh714ct, bi.fldGrpCVh714dt});
 
     }
 
