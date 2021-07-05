69e28c7f382d7a101bfbae56a0a12765d98ca33d
ahs1n <ahsanpervez74@gmail.com>
2021-07-05 12:21:00 +0500

    SectionH6Activity Add Skips

--- app/src/main/java/edu/aku/hassannaqvi/tajik_anemia_study/ui/sections/SectionH6Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/tajik_anemia_study/ui/sections/SectionH6Activity.java
@@ -2,6 +2,7 @@ package edu.aku.hassannaqvi.tajik_anemia_study.ui.sections;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +23,7 @@ import edu.aku.hassannaqvi.tajik_anemia_study.models.Form;
 import edu.aku.hassannaqvi.tajik_anemia_study.ui.EndingActivity;
 
 import static edu.aku.hassannaqvi.tajik_anemia_study.core.MainApp.form;
+import static edu.aku.hassannaqvi.tajik_anemia_study.utils.DateUtilsKt.rgLsnr;
 
 
 public class SectionH6Activity extends AppCompatActivity {
@@ -39,6 +41,16 @@ public class SectionH6Activity extends AppCompatActivity {
 
 
     private void setupSkips() {
+
+        rgLsnr(bi.h601, bi.h601b, new ViewGroup[]{bi.fldGrpCVh602});
+        rgLsnr(bi.h603, bi.h603b, new ViewGroup[]{bi.fldGrpCVh604});
+        rgLsnr(bi.h605, bi.h605b, new ViewGroup[]{bi.fldGrpCVh606});
+        rgLsnr(bi.h607, bi.h607b, new ViewGroup[]{bi.fldGrpCVh608});
+        rgLsnr(bi.h609, bi.h609b, new ViewGroup[]{bi.fldGrpCVh610});
+        rgLsnr(bi.h611, bi.h611b, new ViewGroup[]{bi.fldGrpCVh612});
+        rgLsnr(bi.h613, bi.h613b, new ViewGroup[]{bi.fldGrpCVh614});
+        rgLsnr(bi.h615, bi.h615b, new ViewGroup[]{bi.fldGrpCVh616});
+        rgLsnr(bi.h617, bi.h617b, new ViewGroup[]{bi.fldGrpCVh618});
 
     }
 
