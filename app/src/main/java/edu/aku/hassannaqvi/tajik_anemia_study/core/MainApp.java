b7f62a76697533617398249aeb162136e4bb5334
shznaqvi <shznaqvi@gmail.com>
2021-07-05 14:56:59 +0500

    url set

--- app/src/main/java/edu/aku/hassannaqvi/tajik_anemia_study/core/MainApp.java
+++ app/src/main/java/edu/aku/hassannaqvi/tajik_anemia_study/core/MainApp.java
@@ -23,14 +23,14 @@ public class MainApp extends Application {
     public static final String PROJECT_NAME = "PSBI TRIAL";
     public static final String DIST_ID = null;
     public static final String SYNC_LOGIN = "sync_login";
-    public static final String _IP = "https://vcoe1.aku.edu";// .LIVE server
-    //public static final String _IP = "http://f38158/prosystem";// .TEST server
+    //public static final String _IP = "https://vcoe1.aku.edu";// .LIVE server
+    public static final String _IP = "http://f38158/prosystem";// .TEST server
     //public static final String _IP = "http://43.245.131.159:8080";// .TEST server
-    public static final String _HOST_URL = MainApp._IP + "/psbi/api/";// .TEST server;
+    public static final String _HOST_URL = MainApp._IP + "/anemia_study_tj/api/";// .TEST server;
     public static final String _SERVER_URL = "sync.php";
     public static final String _SERVER_GET_URL = "getData.php";
     public static final String _PHOTO_UPLOAD_URL = _HOST_URL + "uploads.php";
-    public static final String _UPDATE_URL = MainApp._IP + "/psbi/app/";
+    public static final String _UPDATE_URL = MainApp._IP + "/anemia_study_tj/app/";
     public static File sdDir;
     public static String[] downloadData;
     public static Form form;
