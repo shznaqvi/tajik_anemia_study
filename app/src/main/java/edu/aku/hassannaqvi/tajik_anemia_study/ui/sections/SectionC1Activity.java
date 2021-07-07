2cb9ad9c9e15b23597c84347df36aab250a8c474
ahs1n <ahsanpervez74@gmail.com>
2021-07-07 15:03:00 +0500

    SectionC1Activity Add Skips

--- app/src/main/java/edu/aku/hassannaqvi/tajik_anemia_study/ui/sections/SectionC1Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/tajik_anemia_study/ui/sections/SectionC1Activity.java
@@ -2,11 +2,14 @@ package edu.aku.hassannaqvi.tajik_anemia_study.ui.sections;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import com.validatorcrawler.aliazaz.Clear;
 import com.validatorcrawler.aliazaz.Validator;
 
 import java.text.SimpleDateFormat;
@@ -22,6 +25,7 @@ import edu.aku.hassannaqvi.tajik_anemia_study.models.Form;
 import edu.aku.hassannaqvi.tajik_anemia_study.ui.EndingActivity;
 
 import static edu.aku.hassannaqvi.tajik_anemia_study.core.MainApp.form;
+import static edu.aku.hassannaqvi.tajik_anemia_study.utils.DateUtilsKt.rgLsnr;
 
 
 public class SectionC1Activity extends AppCompatActivity {
@@ -39,6 +43,62 @@ public class SectionC1Activity extends AppCompatActivity {
 
 
     private void setupSkips() {
+
+        rgLsnr(bi.c101, bi.c101b, new ViewGroup[]{bi.fldGrpCVc102});
+
+        bi.c106b.setOnCheckedChangeListener((radioGroup, i) -> {
+            bi.fldGrpCVc106c.setVisibility(View.VISIBLE);
+            Clear.clearAllFields(bi.fldGrpCVc106c);
+            if (i == bi.c106bb.getId() || i == bi.c106b98.getId()) {
+                bi.fldGrpCVc106c.setVisibility(View.GONE);
+            }
+        });
+
+        bi.c106d.setOnCheckedChangeListener((radioGroup, i) -> {
+            bi.fldGrpCVc106e.setVisibility(View.VISIBLE);
+            bi.fldGrpCVc106f.setVisibility(View.VISIBLE);
+            Clear.clearAllFields(bi.fldGrpCVc106e);
+            Clear.clearAllFields(bi.fldGrpCVc106f);
+            if (i == bi.c106db.getId() || i == bi.c106d98.getId()) {
+                bi.fldGrpCVc106e.setVisibility(View.GONE);
+                bi.fldGrpCVc106f.setVisibility(View.GONE);
+            }
+        });
+
+        bi.c106g.setOnCheckedChangeListener((radioGroup, i) -> {
+            bi.fldGrpCVc106h.setVisibility(View.VISIBLE);
+            bi.fldGrpCVc106i.setVisibility(View.VISIBLE);
+            Clear.clearAllFields(bi.fldGrpCVc106h);
+            Clear.clearAllFields(bi.fldGrpCVc106i);
+            if (i == bi.c106gb.getId() || i == bi.c106g98.getId()) {
+                bi.fldGrpCVc106h.setVisibility(View.GONE);
+                bi.fldGrpCVc106i.setVisibility(View.GONE);
+            }
+        });
+
+        bi.c106m.setOnCheckedChangeListener((radioGroup, i) -> {
+            bi.fldGrpCVc106n.setVisibility(View.VISIBLE);
+            Clear.clearAllFields(bi.fldGrpCVc106n);
+            if (i == bi.c106mb.getId() || i == bi.c106m98.getId()) {
+                bi.fldGrpCVc106n.setVisibility(View.GONE);
+            }
+        });
+
+        bi.c106p.setOnCheckedChangeListener((radioGroup, i) -> {
+            bi.fldGrpCVc106q.setVisibility(View.VISIBLE);
+            Clear.clearAllFields(bi.fldGrpCVc106q);
+            if (i == bi.c106pb.getId() || i == bi.c106p98.getId()) {
+                bi.fldGrpCVc106q.setVisibility(View.GONE);
+            }
+        });
+
+        bi.c107a.setOnCheckedChangeListener((radioGroup, i) -> {
+            bi.fldGrpCVc107bt.setVisibility(View.VISIBLE);
+            Clear.clearAllFields(bi.fldGrpCVc107bt);
+            if (i == bi.c107ab.getId() || i == bi.c107a98.getId()) {
+                bi.fldGrpCVc107bt.setVisibility(View.GONE);
+            }
+        });
 
     }
 
@@ -88,7 +148,14 @@ public class SectionC1Activity extends AppCompatActivity {
 
 
     private boolean formValidation() {
-        return Validator.emptyCheckingContainer(this, bi.GrpName);
+        if (!Validator.emptyCheckingContainer(this, bi.GrpName))
+            return false;
+
+        if (bi.c108a.isChecked() && (!bi.c107aa.isChecked() || !bi.c107ra.isChecked())) {
+            Toast.makeText(this, "Invalid Response", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 
 
